83b8424c462e584d104214510b2ec7873bb545da
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2025-08-09 13:26:13 +0200

    Responsive header

--- src/components/header/HeaderInfoComponent.jsx
+++ src/components/header/HeaderInfoComponent.jsx
@@ -13,22 +13,23 @@ const HeaderInfoComponent = () => {
     return (
         <div className=" wrapper" >
             {toggle &&
-                <div className="py-2.5 flex-centar-between text-darkBlue ">
+                <div className="py-2.5 flex flex-col gap-2 items-center lg:flex-row lg:justify-between text-darkBlue ">
                     <p>Need Help? Call us: (+98) 0123 456 789</p>
-                    <div className="flex-centar-between gap-3">
+                    {/*  */}
+                    <div className="flex flex-col items-center lg:flex-row gap-3">
                         <div className="flex-centar-between gap-1">
                             <TbMap2 size={20} /> <p>Track Your Delivery</p>
                         </div>
                         <div className="flex-centar-between gap-1">
                             <IoLocationSharp size={20} /> <p>Our Store</p>
                         </div>
-                    <div className="cursor-pointer hover:scale-[1.2] justify-self-end" onClick={() => setToggle(false)}>
-                        <MdKeyboardArrowUp size={30} />
-                    </div>
+                        <div className="cursor-pointer hover:scale-[1.2] justify-self-center lg:justify-self-end" onClick={() => setToggle(false)}>
+                            <MdKeyboardArrowUp size={30} />
+                        </div>
                     </div>
                 </div>
             }
-            {!toggle && <MdKeyboardArrowDown size={30} onClick={() => setToggle(true)} className="justify-self-end cursor-pointer hover:scale-[1.2]"/>}
+            {!toggle && <MdKeyboardArrowDown size={30} onClick={() => setToggle(true)} className="justify-self-center lg:justify-self-end cursor-pointer hover:scale-[1.2]" />}
         </div>
     )
 }
--- src/components/header/NavbarComponent.jsx
+++ src/components/header/NavbarComponent.jsx
@@ -8,18 +8,23 @@ import { IoPersonSharp } from "react-icons/io5";
 const NavbarComponent = () => {
     return (
         <div className="bg-darkBlue text-lightBlue">
-            <div className="wrapper h-[100px] flex-centar-between">
-                <div className="flex-centar-between gap-7">
-                    <img src={logo} alt="logo" />
-                    <div className="bg-lightBlue text-darkBlue rounded-3xl ">
+            <div className="wrapper py-6 flex flex-col justify-between gap-6 items-center lg:flex-row">
+                {/* logo and search */}
+                <div className="flex flex-col items-center lg:flex-row gap-10">
+                    <img src={logo} alt="logo" className="w-[200px] mx-auto" />
+
+                    {/* search */}
+                    <div className="bg-lightBlue text-darkBlue rounded-2xl ">
                         <input type="text" className="px-6 py-2.5 placeholder:text-darkBlue focus:outline-0" placeholder="Search products" />
-                        <button className="px-6 py-2.5 bg-orange rounded-3xl font-semibold cursor-pointer">Search</button>
+                        <button className="px-6 py-2.5 bg-orange rounded-r-2xl text-darkBlue font-semibold cursor-pointer">Search</button>
                     </div>
                 </div>
+
+                {/* navigation */}
                 <nav className="flex-centar-between gap-8">
-                    <div className="flex-centar-between gap-1">< FaShoppingCart /> <Link>Cart</Link></div>
-                    <div className="flex-centar-between gap-1">< FaHeart/> <Link>Favorite</Link></div>
-                    <div className="flex-centar-between gap-1">< IoPersonSharp/> <Link>Profile</Link></div>
+                    <div className="flex-centar-between gap-1"><Link className="flex-centar-between gap-1">< FaShoppingCart size={20} /> Cart</Link><span className="bg-orange px-1 rounded-2xl text-darkBlue">0</span></div>
+                    <div className="flex-centar-between gap-1"><Link className="flex-centar-between gap-1">< FaHeart size={20}/> Favorite</Link><span className="bg-orange px-1 rounded-2xl text-darkBlue">0</span></div>
+                    <div className="flex-centar-between gap-1">< IoPersonSharp size={20}/> <Link>Profile</Link></div>
                 </nav>
             </div>
         </div>
